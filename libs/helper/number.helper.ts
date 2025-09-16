3ce89f0e828fc23df7f2a1c44fb64c8f37bce7f0
khoaxuantu <ngxktuzkai2000@gmail.com>
2025-09-16 23:21:33 +0700

    nuke

--- libs/helper/number.helper.ts
+++
@@ -1,9 +0,0 @@
-export class NumberHelper {
-  static randomInt(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  static randomArbitrary(min: number, max: number) {
-    return Math.random() * (max - min) + min;
-  }
-}
